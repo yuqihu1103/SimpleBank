c3095b26cbbe0c8f210f0915a69b3751535889ef
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-09-26 22:14:49 -0700

    Updated list of supported currency

--- util/currency.go
+++ util/currency.go
@@ -2,11 +2,22 @@ package util
 
 // all supported currency
 const (
-	USD = "USD"
-	CAD = "CAD"
-	GBP = "GBP"
-	CNY = "CNY"
-	RMB = "RMB"
+	USD = "USD" // United States Dollar
+	EUR = "EUR" // Euro
+	JPY = "JPY" // Japanese Yen
+	GBP = "GBP" // British Pound Sterling
+	AUD = "AUD" // Australian Dollar
+	CAD = "CAD" // Canadian Dollar
+	CHF = "CHF" // Swiss Franc
+	CNY = "CNY" // Chinese Yuan
+	SEK = "SEK" // Swedish Krona
+	NZD = "NZD" // New Zealand Dollar
+	NOK = "NOK" // Norwegian Krone
+	SGD = "SGD" // Singapore Dollar
+	KRW = "KRW" // South Korean Won
+	INR = "INR" // Indian Rupee
+	BRL = "BRL" // Brazilian Real
+	RMB = "RMB" // Chinese RenMinBi
 )
 
 func IsSupportedCurrency(currency string) bool {
